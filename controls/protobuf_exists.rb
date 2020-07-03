64d8ea77d53638e708ac1d5527daab85b29dd82e
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-07-03 10:33:26 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/protobuf_exists.rb
+++ controls/protobuf_exists.rb
@@ -7,7 +7,9 @@ control 'core-plans-protobuf-exists' do
   impact 1.0
   title 'Ensure protobuf exists'
   desc '
-  Verify protobuf by ensuring bin/protoc exists'
+  Verify protobuf by ensuring bin/protoc 
+  (1) exists and
+  (2) is executable'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
@@ -16,9 +18,9 @@ control 'core-plans-protobuf-exists' do
     its('stderr') { should be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: 'bin/protoc')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", "protoc")
   describe file(command_full_path) do
     it { should exist }
+    it { should be_executable }
   end
 end
