8978e24ea0b4dfc0c29a422a7974be422a73253d
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-03-26 18:11:24 +0530

    pkg_shasum updated

--- plan.sh
+++ plan.sh
@@ -6,7 +6,7 @@ pkg_upstream_url="https://developers.google.com/protocol-buffers/"
 pkg_license=('BSD')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/google/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-all-${pkg_version}.tar.gz"
-pkg_shasum=a5d77c31014b6246c0167c9b847b1e19e360088de933a07efd42f27079ab008b
+pkg_shasum=a96d66a29df73991ece4d82f04abf242d28f4cdacd7eb0ddf47f75a344a290af
 pkg_deps=(
   core/gcc
   core/zlib
